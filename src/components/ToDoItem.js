28338ec088b205e5a4bbd76f83dfc7873012c3d1
hychoi <hayeong28@naver.com>
2020-06-14 02:43:07 +0900

    make ToDoItem Component

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { MdDone, MdDelete } from 'react-icons/md';
+
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
+
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
+const Remove = styled.div`
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+`;
+
+const ToDoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+
+  &:hover {
+    ${Remove} {
+      opacity: 1;
+    }
+  }
+`;
+
+function ToDoItem({ id, done, text }) {
+  return (
+    <ToDoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove>
+        <MdDelete />
+      </Remove>
+    </ToDoItemBlock>
+  );
+}
+
+export default ToDoItem;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import ToDoItem from './ToDoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -9,7 +10,14 @@ const TodoListBlock = styled.div`
 `;
 
 function ToDoList() {
-  return <TodoListBlock>ToDoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <ToDoItem text="프로젝트 생성하기" done={true} />
+      <ToDoItem text="컴포넌트 스타일링 하기" done={true} />
+      <ToDoItem text="Context 만들기" done={false} />
+      <ToDoItem text="기능 구현하기" done={false} />
+    </TodoListBlock>
+  );
 }
 
 export default ToDoList;
