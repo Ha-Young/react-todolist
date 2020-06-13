ba63a052cd779ddca4f9d3ac4eac78aef652ed79
hychoi <hayeong28@naver.com>
2020-06-14 03:06:08 +0900

    ToDoCreate make Insert, Input UI

--- src/components/ToDoCreate.js
+++ src/components/ToDoCreate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
 
@@ -30,13 +30,64 @@ const CircleButtton = styled.button`
 
   border: none;
   outline: none;
+
+  transition: 0.125s all ease-in;
+  ${(props) =>
+    props.open &&
+    css`
+      background: #ff6b6b;
+      &:hover {
+        background: #ff8787;
+      }
+      &:active {
+        background: #fa5252;
+      }
+      transform: translate(-50%, 50%) rotate(45deg);
+    `}
 `;
 
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+const InsertForm = styled.div`
+  background: #f8f9fa;
+  padding: 32px;
+  padding-bottom: 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
 function ToDoCreate() {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => setOpen(!open);
+
   return (
-    <CircleButtton>
-      <MdAdd />
-    </CircleButtton>
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm>
+            <Input placeholder="할 일을 입력 후, Enter 를 누르세요" autoFocus />
+          </InsertForm>
+        </InsertFormPositioner>
+      )}
+      <CircleButtton onClick={onToggle} open={open}>
+        <MdAdd />
+      </CircleButtton>
+    </>
   );
 }
 
