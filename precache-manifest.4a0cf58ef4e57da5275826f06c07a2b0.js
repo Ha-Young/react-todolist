ed379a2e20e801a9c20ef2e838b90f2568fc5e72
hayeong choi <hayeong28@naver.com>
2020-08-03 09:38:53 +0900

    Updates

---
+++ precache-manifest.4a0cf58ef4e57da5275826f06c07a2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11df99f7640cf9d3dc7e3430b4d0281e",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "e533ed43c6e407dc1bf9",
+    "url": "/react-todolist/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "341bd5cf5986251a77c0",
+    "url": "/react-todolist/static/js/2.78893668.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist/static/js/2.78893668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e533ed43c6e407dc1bf9",
+    "url": "/react-todolist/static/js/main.dd8c18d7.chunk.js"
+  },
+  {
+    "revision": "bdb1eb0d6159838c9f4b",
+    "url": "/react-todolist/static/js/runtime-main.6228bffe.js"
+  }
+]);
